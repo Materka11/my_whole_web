debb451a3c47a70f85126f374d952e8e3bd60e28
Materka11 <arekmaterka11@gmail.com>
2023-04-29 13:26:57 +0200

    Added experience and changed view for aboutMe

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,73 +9,95 @@ import stairs from '../img/Component 7 – 1@2x.png';
 import { useHasScrolled } from '../hooks/useHasScrolled';
 
 function AboutMe() {
-	const [ classContainer, setClassContainer ] = useState('');
+  const [classContainer, setClassContainer] = useState('');
 
-	const scrollAboutMe = useHasScrolled(440);
+  const scrollAboutMe = useHasScrolled(440);
 
-	useEffect(
-		() => {
-			const { innerWidth } = window;
-			if (innerWidth >= 1440) {
-				if (scrollAboutMe) {
-					setClassContainer('active');
-				} else if (!scrollAboutMe) {
-					setClassContainer('');
-				}
-			}
-		},
-		[ document.documentElement.scrollTop ]
-	);
-	return (
-		<div className="aboutMe">
-			<div className={`container ${classContainer}`}>
-				<span>History</span>
-				<p>
-					I've been learning frontend development since October 2017 and have applied my knowledge by working
-					on various projects. My goal was to add more dynamic elements to my pages, so I decided to learn my
-					first language, which was JavaScript and used it to create a basic game. Afterwards, I explored the
-					powerful library React, and after six months of studying it, I created my first React project, which
-					was an online store, also learning Git and Adobe XD in the process. Then, I moved on to Typescript
-					and used it to build this website. To enhance the site further, I added a 3D component using React
-					Three Fiber, requiring me to learn Blender as well. In early 2023, as team member DeveloArt, I
-					worked on developing mobile applications for restaurants using React Native.
-				</p>
-			</div>
-			<div className={`container skill ${classContainer}`}>
-				<span>Skill</span>
-				<ul>
-					<li>Very good knowledge of HTML5, CSS3 and JavaScript (ES6)</li>
-					<li>Good knowledge of React 17</li>
-					<li>Ability to build an application with React 17</li>
-					<li>Ability to use TypeScript</li>
-					<li>Ability to use the Git version control system</li>
-					<li>Ability to use Sass</li>
-					<li>Basic knowledge of PHP and MySQL</li>
-					<li>Basic design skills in Adobe XD</li>
-					<li>Basic design skills in Blender</li>
-					<li>Ability to use React Three Fiber</li>
-					<li>Ability to use the tool: Webpack, Babel</li>
-					<li>Knowledge of React Native</li>
-					<li>Ability to using the styled components library</li>
-					<li>Familiarity with Jira Software</li>
-					<li>
-						I have experience conducting code reviews, including evaluating the quality and readability of
-						code, identifying potential issues, and making suggestions for code improvement. This allows me
-						to ensure high-quality and reliable code and assist my fellow programmers in developing their
-						skills.
-					</li>
-					<li>
-						I have the ability and experience to work in a team of programmers, collaborating with them on
-						common projects and delivering solutions that meet the client's requirements and expectations.
-					</li>
-				</ul>
-			</div>
-			<img className="circle1" src={circles} alt="background element" />
-			<img className="circle2" src={circles} alt="background element" />
-			<img className="stairs1 stairs" src={stairs} alt="background element" />
-			<img className="stairs2 stairs" src={stairs} alt="background element" />
-		</div>
-	);
+  useEffect(() => {
+    const { innerWidth } = window;
+    if (innerWidth >= 1440) {
+      if (scrollAboutMe) {
+        setClassContainer('active');
+      } else if (!scrollAboutMe) {
+        setClassContainer('');
+      }
+    }
+  }, [document.documentElement.scrollTop]);
+  return (
+    <div className="aboutMe">
+      <div className="containerOuter">
+        <div className={`container ${classContainer}`}>
+          <h2>History</h2>
+          <p>
+            I've been learning frontend development since October 2017 and have
+            applied my knowledge by working on various projects. My goal was to
+            add more dynamic elements to my pages, so I decided to learn my
+            first language, which was JavaScript and used it to create a basic
+            game. Afterwards, I explored the powerful library React, and after
+            six months of studying it, I created my first React project, which
+            was an online store, also learning Git and Adobe XD in the process.
+            Then, I moved on to Typescript and used it to build this website. To
+            enhance the site further, I added a 3D component using React Three
+            Fiber, requiring me to learn Blender as well. In October 2022, I
+            started studying at the Koszalin University of Technology to further
+            expand my knowledge and skills in the field of computer science and
+            engineering. Additionally, in early 2023, as team member DeveloArt,
+            I worked on developing mobile applications for restaurants using
+            React Native.
+          </p>
+        </div>
+        <div className={`container skill ${classContainer}`}>
+          <h2>Skills</h2>
+          <ul>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>CSS</li>
+            <li>SASS</li>
+            <li>Styled Components</li>
+            <li>React Native</li>
+            <li>Jira</li>
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>Git</li>
+            <li>JavaScript</li>
+            <li>React Context API</li>
+            <li>Redux</li>
+            <li>Next.js</li>
+            <li>Code Review</li>
+            <li>Team programming</li>
+          </ul>
+        </div>
+        <div className={`container experience ${classContainer}`}>
+          <h2>Experience</h2>
+          <h3>
+            <strong>DeveloArt</strong> / Trainee Frontend Developer
+          </h3>
+          <span>September 2022 - present</span>
+          <div>
+            <p>
+              Creating a mobile application in React Native was my main
+              responsibility, which included:
+            </p>
+            <ul>
+              <li>designing and implementing application screens</li>
+              <li>writing application logic</li>
+              <li>fetching data from the backend,</li>
+              <li>conducting code reviews</li>
+              <li>participating in weekly meetings</li>
+            </ul>
+            <strong>
+              Technologies used: React Native, TypeScript, CSS, JavaScript,
+              Redux
+            </strong>
+          </div>
+        </div>
+        <img className="circle1" src={circles} alt="background element" />
+        <img className="circle2" src={circles} alt="background element" />
+        <img className="stairs1 stairs" src={stairs} alt="background element" />
+        <img className="stairs2 stairs" src={stairs} alt="background element" />
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
