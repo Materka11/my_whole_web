d97dfaffb42eada1f266c00d767034cb823bdbcb
Materka11 <arekmaterka11@gmail.com>
2024-07-05 12:27:14 +0200

    Feature: Camera zoom

--- src/App3D.tsx
+++ src/App3D.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Scene } from './Scene';
-import { Canvas } from '@react-three/fiber';
-import { LoadingScreen } from './LoadingScreen';
-import './styles/_global.scss';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Scene } from "./Scene";
+import { Canvas } from "@react-three/fiber";
+import { LoadingScreen } from "./LoadingScreen";
+import "./styles/_global.scss";
+import { BrowserRouter } from "react-router-dom";
 
 const App3D = () => (
   <React.Fragment>
     <Canvas
       style={{
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        top: '0',
-        left: '0',
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        top: "0",
+        left: "0",
       }}
-      camera={{ position: [-100, 50, 200], zoom: 1.25 }}
+      camera={{ position: [-100, 50, 200], zoom: 3 }}
     >
       <BrowserRouter>
         <Scene />
