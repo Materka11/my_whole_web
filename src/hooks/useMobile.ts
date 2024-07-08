b2e75b1e2711bb199924ccdcf8cc1288f3911b21
Materka11 <arekmaterka11@gmail.com>
2024-07-08 13:13:33 +0200

    Fix: Styles for tablet

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 
+const RESOLUTION = 1440;
+
 export const useMobile = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < RESOLUTION);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
+      setIsMobile(window.innerWidth < RESOLUTION);
     };
 
     window.addEventListener("resize", handleResize);
