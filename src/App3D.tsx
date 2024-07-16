d991b28dc10762af3a16403c32bfb4d78de684e0
Materka11 <arekmaterka11@gmail.com>
2024-07-16 14:07:18 +0200

    Feature: Loader

--- src/App3D.tsx
+++ src/App3D.tsx
@@ -4,25 +4,39 @@ import { Canvas } from "@react-three/fiber";
 import { LoadingScreen } from "./LoadingScreen";
 import "./styles/_global.scss";
 import { BrowserRouter } from "react-router-dom";
+import { Html, useProgress } from "@react-three/drei";
+import Loader from "./Loader";
 
-const App3D = () => (
-  <React.Fragment>
-    <Canvas
-      style={{
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        top: "0",
-        left: "0",
-      }}
-      camera={{ position: [-100, 50, 200], zoom: 3 }}
-    >
-      <BrowserRouter>
-        <Scene />
-      </BrowserRouter>
-    </Canvas>
-    <LoadingScreen />
-  </React.Fragment>
-);
+const App3D = () => {
+  const progress = useProgress();
 
+  const loaded = progress.loaded >= 5;
+
+  return (
+    <React.Fragment>
+      <Canvas
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          backgroundColor: "#000",
+        }}
+        camera={{ position: [-100, 50, 200], zoom: 3 }}
+      >
+        <BrowserRouter>
+          {!loaded ? (
+            <Html center>
+              <Loader />
+            </Html>
+          ) : (
+            <Scene />
+          )}
+        </BrowserRouter>
+      </Canvas>
+      {loaded && <LoadingScreen />}
+    </React.Fragment>
+  );
+};
 export default App3D;
