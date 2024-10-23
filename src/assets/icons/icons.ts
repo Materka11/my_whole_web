9f2cb37576821a0cde7928289c4fb44656dd96ff
Materka11 <arekmaterka11@gmail.com>
2024-10-23 12:19:25 +0200

    Feature: Added raw icons

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,7 @@
+import { GithubIcon } from "./GithubIcon";
+import { GitlabIcon } from "./GitlabIcon";
+import { FacebookIcon } from "./FacebookIcon";
+import { InstagramIcon } from "./InstagramIcon";
+import { LinkedinIcon } from "./LinkedinIcon";
+
+export { GithubIcon, GitlabIcon, FacebookIcon, InstagramIcon, LinkedinIcon };
