a44d88d8a1eb51b39d855d9609e9e5baf63e860c
Materka11 <arekmaterka11@gmail.com>
2024-10-24 11:21:04 +0200

    Feature: Added message icon

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -3,5 +3,13 @@ import { GitlabIcon } from "./GitlabIcon";
 import { FacebookIcon } from "./FacebookIcon";
 import { InstagramIcon } from "./InstagramIcon";
 import { LinkedinIcon } from "./LinkedinIcon";
+import { MessageIcon } from "./MessageIcon";
 
-export { GithubIcon, GitlabIcon, FacebookIcon, InstagramIcon, LinkedinIcon };
+export {
+  GithubIcon,
+  GitlabIcon,
+  FacebookIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  MessageIcon,
+};
