0ead0a6cc77b4e1d273dcb427a4bf0c14474b5d3
Materka11 <arekmaterka11@gmail.com>
2024-10-28 10:25:41 +0100

    Feature: Embodying logic from menu into container logo

---
+++ src/components/ContainerLogoMenu/ContainerLogoMenu.tsx
@@ -0,0 +1,61 @@
+import styles from "./ContainerLogoMenu.module.scss";
+import { MobileMenu } from "../MobileMenu/MobileMenu";
+import { Dispatch, SetStateAction, useState } from "react";
+import logoMenu from "../../assets/img/logoMenu.png";
+import logo from "../../assets/img/logo.png";
+import { ClassNav } from "../Menu/Menu";
+
+interface IProps {
+  switchLogo: string;
+  setClassNoScroll: Dispatch<SetStateAction<string>>;
+  setSwitchLogo: Dispatch<SetStateAction<string>>;
+  setClassNav: Dispatch<SetStateAction<string | number>>;
+  scrollToComponent: (vh: number, px?: number) => void;
+}
+
+export const ContainerLogoMenu = ({
+  switchLogo,
+  setClassNoScroll,
+  setSwitchLogo,
+  setClassNav,
+  scrollToComponent,
+}: IProps) => {
+  const [isActiveMobileMenu, setIsActiveMobileMenu] = useState(false);
+  const [isToggledMenu, setIsToggledMenu] = useState(false);
+
+  const handleClickMenu = () => {
+    if (!isToggledMenu) {
+      setIsActiveMobileMenu(true);
+      setIsToggledMenu(true);
+      setClassNoScroll("noScroll");
+      setSwitchLogo(logoMenu);
+      setClassNav(ClassNav.navMenu);
+    } else {
+      setIsActiveMobileMenu(false);
+      setIsToggledMenu(false);
+      setClassNoScroll("");
+      setSwitchLogo(logo);
+      setClassNav("");
+    }
+  };
+  return (
+    <>
+      <div className={styles.containerMenu}>
+        <img className={styles.logo} src={switchLogo} alt="logo" />
+        <span className={styles.spaceContainer}>
+          <hr className={styles.space} />
+        </span>
+        <span className={styles.spanMenu} onClick={handleClickMenu}>
+          <span>CLOSE</span>
+          <span className={styles.menuSpan}>MENU</span>
+        </span>
+      </div>
+
+      <MobileMenu
+        isActiveMobileMenu={isActiveMobileMenu}
+        handleClickMenu={handleClickMenu}
+        scrollToComponent={scrollToComponent}
+      />
+    </>
+  );
+};
