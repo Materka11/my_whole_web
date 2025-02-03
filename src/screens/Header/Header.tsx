a45d2530138d78869bf7bd170906cfce9649ea29
Materka11 <arekmaterka11@gmail.com>
2025-02-03 11:11:26 +0100

    Feature: Add Name component with styling and integrate into Header

--- src/screens/Header/Header.tsx
+++ src/screens/Header/Header.tsx
@@ -1,9 +1,9 @@
 import { Dispatch, SetStateAction } from "react";
 import styles from "./Header.module.scss";
-import background from "../../assets/img/Component 12 – 1@2x2.png";
 import { Menu } from "../../components/Menu/Menu";
 import { Bust } from "../../components/Bust/Bust";
 import { DesktopMenu } from "../../components/DesktopMenu";
+import { Name } from "../../components/Name/Name";
 
 interface Props {
   setClassNoScroll: Dispatch<SetStateAction<string>>;
@@ -27,18 +27,11 @@ export const Header = ({ setClassNoScroll }: Props) => {
         setClassNoScroll={setClassNoScroll}
         scrollToComponent={scrollToComponent}
       />
-      <Bust />
+      <section className={styles.content}>
+        <Name />
+        <Bust />
+      </section>
       {/*
-      <div className="background">
-        <img src={background} alt="background" />
-      </div>
-      <span className="name">
-        <strong className="hi">Hi, my</strong> name is <strong>Arek</strong>
-        <span className="description">
-          I'm a <strong>self-taught frontend developer</strong> from Pomorskie,
-          Poland.
-        </span>
-      </span>
       <span className="scroll" />
       <div className="scroll" />
       <DesktopMenu scrollToComponent={scrollToComponent} /> */}
